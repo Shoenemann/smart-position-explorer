07b2af94e95f487bf704fa4d4ab6a6f653c9985e
ghilu91 <ghidelli.luca@gmail.com>
2025-04-03 00:02:06 +0200

    convert list to pgn (chess)

--- src/chess.py
+++ src/chess.py
@@ -13,3 +13,24 @@ def pgn_to_fen(pgn: str):
     game = chess.pgn.read_game(pgnTextIO)
     board = game.end().board()
     return board.fen()
+
+def list_to_pgn(list_moves_str):
+    """
+    example:  
+        input = ["e4", "e5", "Nf3"]
+        output = "1. e4 e5 2. Nf3"
+    """
+    pgn = ""
+    for i, move in enumerate(list_moves_str):
+        if move is None or move == "" or pd.isna(move):
+            break
+        if i%2 == 0:
+            # it is a white move
+            num_move = 1+(i//2)
+            pgn += " "
+            pgn += str(num_move) 
+            pgn += "."
+        pgn +=  " " 
+        pgn +=  move
+        i+=1
+    return pgn
